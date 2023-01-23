017243f15767740559b859d03fe7255400eacfe6
shashank <shashank944533@gmail.com>
2023-01-23 12:38:23 +0530

    instance var first commit

---
+++ instantvariable/src/instantvariable/Program.java
@@ -0,0 +1,17 @@
+package instantvariable;
+
+public class Program {
+
+	int a;
+	float b;
+	boolean c;
+	String d;
+	public static void main(String []args) 
+	{
+		Program p = new Program();
+		System.out.println(p.a);
+		System.out.println(p.b);
+		System.out.println(p.c);
+		System.out.println(p.d);
+}
+}
