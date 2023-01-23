d38cf636b17ccc59b61bcfd599193d35a040650f
shashank <shashank944533@gmail.com>
2023-01-23 12:42:34 +0530

    first commit hashcode snippet

---
+++ hashcode/src/hashcode/Program.java
@@ -0,0 +1,11 @@
+package hashcode;
+
+public class Program 
+{
+public static void main(String []args)
+{
+	String s1 = "kodnest";
+	System.out.println(s1);
+	System.out.println(s1.hashCode());
+}
+}
