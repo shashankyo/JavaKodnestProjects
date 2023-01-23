0ec22c2954364248571aa2a6faa9cc96c48cbbc9
shashank <shashank944533@gmail.com>
2023-01-23 13:00:51 +0530

    first commit string inbuilt func

---
+++ charactersearch/src/charactersearch/Program.java
@@ -0,0 +1,16 @@
+package charactersearch;
+
+public class Program {
+public static void main(String []args)
+{
+	String s1 = "kodnest";
+	System.out.println(s1);
+	System.out.println( s1.charAt(5));
+	System.out.println(s1.toUpperCase());
+	System.out.println(s1.toLowerCase());
+	System.out.println(s1.startsWith("katrina"));
+	System.out.println(s1.startsWith("kodnest"));
+	System.out.println(s1.contains("alai"));
+	System.out.println(s1.indexOf('s'));
+}
+}
