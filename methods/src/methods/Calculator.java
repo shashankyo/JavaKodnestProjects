d604dab5afdc1ab224f74f14f6d7733eb0a01aad
shashank <shashank944533@gmail.com>
2023-01-23 12:49:24 +0530

    first commit calci

---
+++ methods/src/methods/Calculator.java
@@ -0,0 +1,25 @@
+package methods;
+
+public class Calculator {
+	void add() { 
+		int a = 10;
+		int b = 20;
+		int c = a + b;
+		System.out.println(c);
+		} 
+	void sub(int a, int b) {
+		int c = a - b;
+		System.out.println(c);
+		} 
+	int mul() { 
+		int a = 10; 
+		int b = 20;
+		int c = a * b;
+		return c;
+		} 
+	int div(int a, int b) { 
+		int c = a / b;
+		return c;
+		}
+	} 
+
---
+++ methods/src/methods/CalculatorApp.java
@@ -0,0 +1,11 @@
+package methods;
+
+class CalculatorApp { 
+	public static void main(String[] args) {
+		Calculator calc = new Calculator();
+		calc.add(); 
+		calc.sub(10, 20);
+		System.out.println(calc.mul());
+		System.out.println(calc.div(20, 5)); 
+		}
+	} 
