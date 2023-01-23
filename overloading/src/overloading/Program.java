4ecd0b4f333d601131dc023c1fda1746e582a785
shashank <shashank944533@gmail.com>
2023-01-23 12:22:06 +0530

    first commit method overloading

---
+++ overloading/src/overloading/Program.java
@@ -0,0 +1,9 @@
+package overloading;
+
+public class Program {
+public static void main(String []args)
+{
+	Program1 P = new Program1();
+	System.out.println(P.add(10,20));
+}
+}
---
+++ overloading/src/overloading/Program1.java
@@ -0,0 +1,12 @@
+package overloading;
+
+public class Program1 {
+	float add(int x, float y)
+	{
+		return x + y;
+	}
+	float add(float x, float y)
+	{
+		return x + y;
+	}
+}
