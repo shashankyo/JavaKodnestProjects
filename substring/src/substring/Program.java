04336412c79da095db3d6cdc63987569e50e8e2c
shashank <shashank944533@gmail.com>
2023-01-23 12:06:12 +0530

    first commit substring

---
+++ substring/src/substring/Program.java
@@ -0,0 +1,12 @@
+package substring;
+
+public class Program {
+
+public static void main(String []args)
+{
+	String s1 = "kodnest technology";
+	System.out.println(s1);
+	System.out.println(s1.substring(3));
+	System.out.println(s1.substring(3,13));
+}
+}
