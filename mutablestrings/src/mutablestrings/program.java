af2dda47a30c36870786d390cad6769180f543a1
shashank <shashank944533@gmail.com>
2023-01-23 12:24:22 +0530

    first commit mutable string

---
+++ mutablestrings/src/mutablestrings/program.java
@@ -0,0 +1,12 @@
+package mutablestrings;
+
+public class program {
+	public static void main(String[] args)
+	{ 
+		StringBuffer sb = new StringBuffer("KodNest"); 
+		System.out.println(sb); //KodNest 
+		sb.append("Tech"); 
+		System.out.println(sb); //KodNestTech } } 
+	}
+	}
+
