4e44376162845adfb742a3d1887343ff0fa605d9
shashank <shashank944533@gmail.com>
2023-01-24 06:33:21 +0530

    first commit

---
+++ trying/src/trying/Program.java
@@ -0,0 +1,48 @@
+package trying;
+
+public class Program {
+public static void main(String []args) 
+{
+	for(int i=0; i<=5;i++)
+	{
+		System.out.println("kodnest");
+	}
+		System.out.println("technology");
+		
+		System.out.println("second op");
+	
+		for(int i=0; i<=5;i++)
+		{
+			System.out.println("kodnest");
+		
+			System.out.println("technology");
+		}
+		
+		System.out.println("third op");
+		
+	int i=0;
+	while(i<=5)
+	{
+		System.out.println("kodnest");
+		i++;
+	}
+	System.out.println("technology");
+
+	int i1= 0;
+	while(i1<=5)
+	{
+		System.out.println(++i1);
+		System.out.println("tech");
+	}
+	System.out.println("fifth op");
+	for(int i11=0;i11<=5;i11++) {
+		System.out.println("kodnest");
+		if(i11==3)
+		{
+			System.out.println("tech");
+			break;
+		}
+		System.out.println("out of it");
+	}
+}
+}
