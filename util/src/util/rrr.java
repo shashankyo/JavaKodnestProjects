0984dfe4c43f32552d3c8da2761d3073ed1e0d24
shashank <shashank944533@gmail.com>
2023-01-24 06:35:02 +0530

    first commit strings

---
+++ util/src/util/rrr.java
@@ -0,0 +1,20 @@
+package util;
+
+public class rrr {
+
+	public static void main(String [] args)
+	{
+		String s1="kodnest";
+		String s2="kodnest";
+		if(s1.equals(s2))
+		{
+			System.out.println("values are equal");
+		}
+		else
+		{
+			System.out.println("values are not equal");
+		}
+	
+	}
+
+} 
