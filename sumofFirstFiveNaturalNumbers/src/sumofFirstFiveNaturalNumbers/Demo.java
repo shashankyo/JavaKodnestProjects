45e623fd4e5ab9c50612bed63ece6b7a2b79baa0
shashank <shashank944533@gmail.com>
2023-02-09 12:28:41 +0530

    first commit sum of first 5 nos

---
+++ sumofFirstFiveNaturalNumbers/src/sumofFirstFiveNaturalNumbers/Demo.java
@@ -0,0 +1,13 @@
+package sumofFirstFiveNaturalNumbers;
+
+public class Demo {
+void sumNaturalNumber()
+{
+	int sum = 0;
+	for(int i=1;i<=5;i++)
+	{
+		sum = sum+ i;
+	}
+	System.out.println("Sum of first 5 natural numbers is " + sum);
+}
+}
---
+++ sumofFirstFiveNaturalNumbers/src/sumofFirstFiveNaturalNumbers/Launch.java
@@ -0,0 +1,8 @@
+package sumofFirstFiveNaturalNumbers;
+
+public class Launch {
+public static void main(String[] args) {
+	Demo demo = new Demo();
+	demo.sumNaturalNumber();
+}
+}
