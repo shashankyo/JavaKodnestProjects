8881b293fb71e535797227090877e70c3b086882
shashank <shashank944533@gmail.com>
2023-02-12 10:27:23 +0530

    first commit No of char in string

---
+++ capacitycalc/src/capacitycalc/Program.java
@@ -0,0 +1,12 @@
+package capacitycalc;
+
+public class Program {
+public static void main(String []args)
+{
+	StringBuffer sb = new StringBuffer();
+	sb.append("Hello World");
+	System.out.println(sb.capacity());
+	sb.append("nishu bro");
+	System.out.println(sb.capacity());
+}
+}
