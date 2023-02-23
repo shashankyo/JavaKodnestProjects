c1aefffcbc1808cf08d369e107266665bda53ee1
shashankyo <114981861+shashankyo@users.noreply.github.com>
2023-02-23 11:07:15 +0530

    Hacker rank anagram

--- Solution.java
+++ Solution.java
@@ -1,23 +1,35 @@
-package hackerPallindrome;
+package hackeranagram;
 
-import java.util.Scanner;
+import java.util.Arrays;
 
 public class Solution {
+public static void main(String[] args) {
+	boolean flag = true;
+	String str1 = "earth";
+	char[] charArray = str1.toCharArray();
+	Arrays.sort(charArray);
 
-	    public static void main(String[] args) {
-	        
-	        Scanner sc=new Scanner(System.in);
-	        String str ="kodnest";	        /* Enter your code here. Print output to STDOUT. */
-	        StringBuilder str1 = new StringBuilder(str);
-	        StringBuilder reverse = str1.reverse();
-	        String string = reverse.toString();
-	       
-	        if(str.compareTo(string)==0)
-	        {
-	        	System.out.println("Yes");
-	        }else{
-	        	System.out.println("No");
-	        }
-	    }
+	
+	String str2 = "na";
+	char[] charArray2 = str2.toCharArray();
+	Arrays.sort(charArray2);
+	
+	flag = true;
+	if (charArray.length == charArray2.length)
+	{
+		for(int i=0;i<str1.length();i++)
+			{
+			if(charArray[i]!=charArray2[i])
+			{
+				flag = false;
+			}
+		}
 	}
-
+	if(flag == true)
+	{
+		System.out.println("Anagram");
+	}else{
+		System.out.println("Not a Anagram");
+	}
+}
+}
