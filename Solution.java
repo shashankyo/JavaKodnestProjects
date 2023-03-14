2c12cfd655aa9f5183b66ea06991bae3d0b98429
shashankyo <114981861+shashankyo@users.noreply.github.com>
2023-03-14 15:18:29 +0530

    leap year check

--- Solution.java
+++ Solution.java
@@ -1,19 +1,38 @@
-package floydsTriangle;
+package leapYearSubQuestion;
+
+import java.util.Scanner;
 
 public class Solution {
 public static void main(String[] args) {
-	int n = 5;
-
-    int i, j, k = 1;
-
-    for (i = 1; i <= n; i++) {
-
-        for (j = 1; j <= i; j++) {
-
-            System.out.print(k + "  ");
-            k++;
-        }
-        System.out.println();
-    }
+	System.out.println("Enter the year");
+	Scanner scan = new Scanner(System.in);
+	int n = scan.nextInt();
+	boolean flag = false;
+//	if(n%400==0 && (n % 100!=0 || n% 4 == 0))
+//	{
+//		System.out.println("Leap year");
+//	}else {
+//		System.out.println("Not a leap year");
+//	}
+	if(n % 400 == 0)
+	{
+		if(n % 100 == 0)
+		{
+			if(n % 4 == 0)
+			{
+				flag = true;
+			}else {
+				flag = false;
+			}
+		}
+	}else {
+		flag = false;
+	}
+	if(flag)
+	{
+		System.out.println("leap");
+	}else {
+		System.out.println("not a leap");
+	}
 }
 }
