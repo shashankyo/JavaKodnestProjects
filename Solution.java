f524895d481c1a79c3796210f4ea6d3a8f3a827f
shashankyo <114981861+shashankyo@users.noreply.github.com>
2023-03-14 15:20:34 +0530

    String length without inbuilt function

--- Solution.java
+++ Solution.java
@@ -1,38 +1,17 @@
-package leapYearSubQuestion;
+package lengthOfStringWithoutInbuilt;
 
 import java.util.Scanner;
 
 public class Solution {
 public static void main(String[] args) {
-	System.out.println("Enter the year");
+	System.out.println("Enter a string");
 	Scanner scan = new Scanner(System.in);
-	int n = scan.nextInt();
-	boolean flag = false;
-//	if(n%400==0 && (n % 100!=0 || n% 4 == 0))
-//	{
-//		System.out.println("Leap year");
-//	}else {
-//		System.out.println("Not a leap year");
-//	}
-	if(n % 400 == 0)
-	{
-		if(n % 100 == 0)
-		{
-			if(n % 4 == 0)
-			{
-				flag = true;
-			}else {
-				flag = false;
-			}
-		}
-	}else {
-		flag = false;
+	String str = scan.next();
+	char ch[] = str.toCharArray();
+	int count = 0;
+	for (char c :ch) {
+	    count++;
 	}
-	if(flag)
-	{
-		System.out.println("leap");
-	}else {
-		System.out.println("not a leap");
-	}
+	System.out.println("length: " + count);
 }
 }
