1494d704a419e129bd9d841ab602ba201ce09c29
shashankyo <114981861+shashankyo@users.noreply.github.com>
2023-03-16 12:14:57 +0530

    swapping of first and last letter of a string

--- Solution.java
+++ Solution.java
@@ -1,49 +1,16 @@
-package sumofDiagonalMatrix;
+package swapFirstLast;
 
 import java.util.Scanner;
 
 public class Solution {
 public static void main(String[] args) {
-	Scanner scan = new Scanner(System.in);
-	
-    int i,j,x,y,sum=0;
-	System.out.println("Enter the number of rows:");
-	x = scan.nextInt();
-	System.out.println("Enter the number of columns:");
-	y = scan.nextInt();
-	
-	int[][] mat = new int[x][y];
-
-	System.out.println("Enter the elements of the matrix") ;
-	for(i=0;i<x;i++)
-	{ 
-		for(j=0;j<y;j++)
-		{ 
-    		mat[i][j] = scan.nextInt();
-		}
-	}
-
-	System.out.println("The elements of the matrix") ;
-	for(i=0;i<x;i++)
-	{ 
-		for(j=0;j<y;j++)
-		{ 
-   	 		System.out.print(mat[i][j]);
-		}
-	 		System.out.println("");
-	}
-
-	for(i=0;i<x;i++)
-	{ 
-		for(j=0;j<y;j++)
-		{
-		if(i==j)	
-		{
-			sum = sum + mat[i][j];
-		}
-		}
-	}
-
-	System.out.printf("SUM of DIAGONAL elements of the matrix = "+sum) ;
-}	
+	System.out.println("ENter a string");
+	Scanner scan =  new Scanner(System.in);
+	String str = scan.next();
+	char ch[] = str.toCharArray();
+	char temp =ch[0];
+	ch[0] = ch[ch.length-1];
+	ch[ch.length-1] = temp;
+	System.out.println(ch);
+}
 }
