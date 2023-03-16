71d5283c8073b0d36ca173f5678ed70f6058bb58
shashankyo <114981861+shashankyo@users.noreply.github.com>
2023-03-16 12:12:16 +0530

    diagonal sum

--- Solution.java
+++ Solution.java
@@ -1,17 +1,49 @@
-package lengthOfStringWithoutInbuilt;
+package sumofDiagonalMatrix;
 
 import java.util.Scanner;
 
 public class Solution {
 public static void main(String[] args) {
-	System.out.println("Enter a string");
 	Scanner scan = new Scanner(System.in);
-	String str = scan.next();
-	char ch[] = str.toCharArray();
-	int count = 0;
-	for (char c :ch) {
-	    count++;
+	
+    int i,j,x,y,sum=0;
+	System.out.println("Enter the number of rows:");
+	x = scan.nextInt();
+	System.out.println("Enter the number of columns:");
+	y = scan.nextInt();
+	
+	int[][] mat = new int[x][y];
+
+	System.out.println("Enter the elements of the matrix") ;
+	for(i=0;i<x;i++)
+	{ 
+		for(j=0;j<y;j++)
+		{ 
+    		mat[i][j] = scan.nextInt();
+		}
 	}
-	System.out.println("length: " + count);
-}
+
+	System.out.println("The elements of the matrix") ;
+	for(i=0;i<x;i++)
+	{ 
+		for(j=0;j<y;j++)
+		{ 
+   	 		System.out.print(mat[i][j]);
+		}
+	 		System.out.println("");
+	}
+
+	for(i=0;i<x;i++)
+	{ 
+		for(j=0;j<y;j++)
+		{
+		if(i==j)	
+		{
+			sum = sum + mat[i][j];
+		}
+		}
+	}
+
+	System.out.printf("SUM of DIAGONAL elements of the matrix = "+sum) ;
+}	
 }
