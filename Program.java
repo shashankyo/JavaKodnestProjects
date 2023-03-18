6a692b579f386599e4e1ec76a4793609443bc7f1
shashankyo <114981861+shashankyo@users.noreply.github.com>
2023-03-18 12:02:53 +0530

    Recursion

--- Program.java
+++ Program.java
@@ -1,23 +1,19 @@
-package standardCode;
-
-import java.util.Scanner;
+package recursion;
 
 public class Program {
 public static void main(String[] args) {
-	System.out.println("Enter array length");
-	Scanner scan  = new Scanner(System.in);
-	int n = scan.nextInt();
-	int array[] = new int[n];
-	System.out.println("Enter array elements");
-	for(int i=0;i<array.length;i++)
-	{
-		 array[i] = scan.nextInt(); 
-	}
-	System.out.println("Entered array elements are:");
-	for(int i=0;i<array.length;i++)
-	{
-		System.out.println(array[i]);
-	}
-	scan.close();
+	display(1);
 }
+
+private static void display(int i) {
+	if (i==10)
+		return;
+	{
+		System.out.println("Hello world");
+		i++;
+		display(i);
+	}
+	
+}
+
 }
