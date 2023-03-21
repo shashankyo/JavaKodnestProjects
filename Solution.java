830b6adc15b7b4dbb9d8f2494290487476ad8226
shashankyo <114981861+shashankyo@users.noreply.github.com>
2023-03-21 15:29:50 +0530

    Printing one zeros in pyramid

--- Solution.java
+++ Solution.java
@@ -1,28 +1,21 @@
-package occurenceOfEachLetter;
-
-import java.util.Scanner;
+package oneZeroPattern;
 
 public class Solution {
-	public static void main(String[] args) {
-		
-	 String x;
-	    Scanner input = new Scanner(System.in);
-	    System.out.println("Enter a string");
-	    x = input.nextLine();
-	    x = x.toUpperCase();
-	    int size = x.length();
-	    for(int i =0;i<size;i++) {
-	        int count=1;
-	        char find = x.charAt(i);
-
-	        for(int j=i+1;j<size;j++) {
-	            if(find == x.charAt(j)) {
-	                count++;
-	            }
-	        }
-
-	        System.out.printf("%c\t%d",x.charAt(i),count);
-	        System.out.println();       
-	    }
+public static void main(String[] args) {
+	for(int i=1;i<=5;i++)
+	{
+		for(int j=1;j<=i;j++)
+		{
+			if((i+j)%2 == 0)
+			{
+				System.out.print("1");
+			}
+			else
+			{
+				System.out.print("0");
+			}
+		}
+		System.out.println();
+	}
 }
 }
