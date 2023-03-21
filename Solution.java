9d4b0e2bf909609f8929d5b241ddd9e9c8fa1e06
shashankyo <114981861+shashankyo@users.noreply.github.com>
2023-03-21 15:23:46 +0530

    factorial with recursion

--- Solution.java
+++ Solution.java
@@ -1,32 +1,12 @@
-package driveStars;
-
-import java.util.Scanner;
+package factorialwithRec;
 
 public class Solution {
 public static void main(String[] args) {
-	System.out.println("Please enter the number");
-	Scanner scan = new Scanner(System.in);
-//	int number = scan.nextInt();
-//	int a[n] = scan.nextInt;
-	int a[] = {10,5,20,20,4,5,2,25,1};
-	int min = a[0];
-	int max = a[0];
-	int mincount = 0;
-	int maxcount =0;
-	for(int i=0;i<a.length;i++)
-	{
-		if(a[i] < min)
-		{
-			min = a[i];
-			mincount++;
-		}
-		if(a[i] > max)
-		{
-			max = a[i];
-			maxcount++;
-		}
-	}
-	System.out.println(mincount);
-	System.out.println(maxcount);
+	int factorial = 0;
+	System.out.println(factorial(6));
+}
+
+private static int factorial(int n) {
+	return (n==0)?1:(n * factorial(n-1));
 }
 }
