da6b410307991a8f39f1296c5be6977e57fdb99d
shashankyo <114981861+shashankyo@users.noreply.github.com>
2023-07-07 14:24:53 +0530

    Update Program.java

--- charactersearch/src/charactersearch/Program.java
+++ charactersearch/src/charactersearch/Program.java
@@ -5,7 +5,7 @@ public static void main(String []args)
 {
 	String s1 = "kodnest";
 	System.out.println(s1);
-	System.out.println( s1.charAt(5));
+	System.out.println(s1.charAt(5));
 	System.out.println(s1.toUpperCase());
 	System.out.println(s1.toLowerCase());
 	System.out.println(s1.startsWith("katrina"));
